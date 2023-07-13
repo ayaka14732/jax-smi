e091c7c959274be82071571d2b58d5dc0d40e34a
Ayaka Mikazuki <ayaka@mail.shn.hk>
2023-07-13 16:48:59 +0800

    Support multiple devices

--- src/jax_smi/cli_tool.py
+++ src/jax_smi/cli_tool.py
@@ -1,18 +1,27 @@
 import curses
 import fire
+from glob import glob
+import os
 import time
 import subprocess
+from typing import NoReturn
 
-def run(interval: float=1., dir_prefix: str='/dev/shm'):
+def sanitise_paragraph(p: str) -> str:
+    return '\n'.join(line for line in p.split('\n') if line)
+
+def pprof_one_file(filename: str, dir_prefix: str) -> str:
+    return filename + '\n' + sanitise_paragraph(subprocess.run(
+        args=['go', 'tool', 'pprof', '-tags', os.path.join(dir_prefix, filename)],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+    ).stdout.decode('utf-8'))
+
+def run(interval: float=1., dir_prefix: str='/dev/shm/jax-smi') -> NoReturn:
     stdscr = curses.initscr()
     try:
         while True:
             stdscr.clear()
-            output = subprocess.run(
-                args=['go', 'tool', 'pprof', '-tags', f'{dir_prefix}/memory.prof'],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.DEVNULL,
-            ).stdout.decode('utf-8')
+            output = '\n\n'.join(pprof_one_file(filepath, dir_prefix=dir_prefix) for filepath in sorted(glob('device-*.prof', root_dir=dir_prefix)))
             stdscr.addstr(output)
             stdscr.refresh()
             time.sleep(interval)
--- src/jax_smi/initialise_tracking.py
+++ src/jax_smi/initialise_tracking.py
@@ -1,13 +1,19 @@
-def initialise_tracking(interval: float=1., dir_prefix: str='/dev/shm') -> None:
+def initialise_tracking(interval: float=1., dir_prefix: str='/dev/shm/jax-smi', rank: str=0) -> None:
     import jax
+    import os
     import threading
+
+    os.makedirs(dir_prefix, exist_ok=True)
+
+    filepath = os.path.join(dir_prefix, f'device-{rank}.prof')
+    filepath_new = f'{filepath}.new'
 
     def inner():
         import posix
         import time
         while True:
-            jax.profiler.save_device_memory_profile(f'{dir_prefix}/memory.prof.new')
-            posix.rename(f'{dir_prefix}/memory.prof.new', f'{dir_prefix}/memory.prof')  # atomic
+            jax.profiler.save_device_memory_profile(filepath_new)
+            posix.rename(filepath_new, filepath)  # atomic
             time.sleep(interval)
 
     thread = threading.Thread(target=inner, daemon=True)
